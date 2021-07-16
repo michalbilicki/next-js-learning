8de6e31981fc2f169a5fdda50dfd41caed6e51e4
michalbilicki <michel.bilicki@gmail.com>
2021-07-16 12:47:30 +0200

    Revert api changes

--- pages/api/items.js
+++ pages/api/items.js
@@ -4,52 +4,50 @@ import { PrismaClient, Prisma } from '@prisma/client';
 const prisma = new PrismaClient();
 
 const get = async (req, res) => {
-  // try {
-  //   if (req.query.page && req.query.perPage) {
-  //     const page = Number(req.query.page);
-  //     const perPage = Number(req.query.perPage);
-  //     const skip = page === 1 ? 0 : page * perPage - perPage;
-  //     const items = await prisma.item.findMany({
-  //       skip: skip,
-  //       take: perPage
-  //     });
-  //     const totalCount = await prisma.item.count();
+  try {
+    if (req.query.page && req.query.perPage) {
+      const page = Number(req.query.page);
+      const perPage = Number(req.query.perPage);
+      const skip = page === 1 ? 0 : page * perPage - perPage;
+      const items = await prisma.item.findMany({
+        skip: skip,
+        take: perPage
+      });
+      const totalCount = await prisma.item.count();
 
-  //     return res.status(200).json({
-  //       message: 'You can access this content because you are signed in.',
-  //       totalCount: totalCount,
-  //       pageCount: Number(
-  //         Number.isInteger(totalCount / perPage)
-  //           ? totalCount / perPage
-  //           : Math.floor(totalCount / perPage) + 1
-  //       ),
-  //       currentPage: Number(page),
-  //       perPage: Number(perPage),
-  //       items: items
-  //     });
-  //   }
-  //   const items = await prisma.item.findMany();
-  //   return res.status(200).json({
-  //     message: 'You can access this content because you are signed in.',
-  //     items: items
-  //   });
-  // } catch (err) {
-  //   console.error(err);
-  //   return res.status(400).json();
-  // }
-  return res.status(200);
+      return res.status(200).json({
+        message: 'You can access this content because you are signed in.',
+        totalCount: totalCount,
+        pageCount: Number(
+          Number.isInteger(totalCount / perPage)
+            ? totalCount / perPage
+            : Math.floor(totalCount / perPage) + 1
+        ),
+        currentPage: Number(page),
+        perPage: Number(perPage),
+        items: items
+      });
+    }
+    const items = await prisma.item.findMany();
+    return res.status(200).json({
+      message: 'You can access this content because you are signed in.',
+      items: items
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(400).json();
+  }
 };
 
 const post = async (req, res) => {
-  // try {
-  //   const itemData = JSON.parse(req.body);
-  //   const savedItem = await prisma.item.create({ data: itemData });
-  //   console.log(savedItem);
-  //   return res.status(200).json(savedItem);
-  // } catch (err) {
-  //   return res.status(400).json();
-  // }
-  return res.status(200);
+  try {
+    const itemData = JSON.parse(req.body);
+    const savedItem = await prisma.item.create({ data: itemData });
+    console.log(savedItem);
+    return res.status(200).json(savedItem);
+  } catch (err) {
+    return res.status(400).json();
+  }
 };
 
 export const items = async (req, res) => {
